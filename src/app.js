1ac73c5e9945d28c3977f12ccb1149efce1cd033
Dongfeng Gu <gudongfeng@outlook.com>
2016-01-20 22:14:11 -0500

    add the new routes (blog)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 'use strict'
 
-var express = require('express');
+var express = require('express'),
+  posts = require('./mock/posts.json');
 
 var app = express();
 
-app.get('/', function(request, response){
+app.get('/', function(request, response) {
   response.send("<h1>I Love Treehouse</h1>");
 });
 
-app.listen(3000, function(){
+app.get('/blog', function(request, response) {
+  response.send(posts);
+});
+
+app.listen(3000, function() {
   console.log("The frontend server is running in port 3000");
 });
