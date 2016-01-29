2a056698161faa0449f3601502430069ccc3d8c8
Dongfeng Gu <gudongfeng@outlook.com>
2016-01-28 21:39:50 -0500

    structure the layout file add the partiles folders to store the  header nav and so on

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
-'use strict'
+'use strict';
 
 var express = require('express'),
-  posts = require('./mock/posts.json');
+	  posts = require('./mock/posts.json');
 
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send("<h1>I Love Treehouse</h1>");
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res){
+	res.render('index');
 });
 
-app.get('/blog', function(request, response) {
-  response.send(posts);
+app.get('/blog/:title?', function(req, res){
+	var title = req.params.title;
+	if (title === undefined) {
+		res.status(503);
+		res.send("This page is under construction!");
+	} else {
+		var post = posts[title] || {};
+		res.render('post', { post: post});
+	}
 });
 
 app.listen(3000, function() {
-  console.log("The frontend server is running in port 3000");
+	console.log("The frontend server is running on port 3000!");
 });
